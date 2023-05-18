db818edc6240de54bc7c44f2c8cf85a48fc3418d
Luana Portezan <luanaportezan@gmail.com>
2023-05-18 05:25:38 -0300

    feat: middleware error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from './Routes/routes';
+import ErrorHandler from './Middlewares/error.handler';
 
 const app = express();
 app.use(express.json());
 
+app.use(ErrorHandler.handle);
 app.use(routes);
 
 export default app;
