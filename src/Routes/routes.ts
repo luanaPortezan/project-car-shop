c7c655242443b668cea9882627dcc0110e848fd8
Luana Portezan <luanaportezan@gmail.com>
2023-05-18 05:53:57 -0300

    feat: router get all cars

--- src/Routes/routes.ts
+++ src/Routes/routes.ts
@@ -5,8 +5,14 @@ const routes = Router();
 
 routes.post(
   '/cars',
-  (req, res, next) => new CarController(req, res, next)
-    .insertOneNewCar(),
+  (req, res, next) => 
+    new CarController(req, res, next)
+      .insertOneNewCar(),
 );
-
+routes.get(
+  '/cars',
+  (req, res, next) => 
+    new CarController(req, res, next)
+      .findAllCar(),
+);
 export default routes;
