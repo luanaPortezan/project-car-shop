fa4295b2b5ceb41be591942534929da0cfd434c2
Luana Portezan <luanaportezan@gmail.com>
2023-05-18 12:29:50 -0300

    refactor: tests services

--- tests/unit/Services/CarService.test.ts
+++ tests/unit/Services/CarService.test.ts
@@ -6,52 +6,60 @@ import { carListGetResponse, carListMongoResponse,
   newCarUpdatedMongoResponse, newCarUpdatedPost } from './mocks/CarMock';
 import CarService from '../../../src/Services/CarService';
 
-describe('Testa a camada CarService', function () {
-  it('Cadastrando novo carro', async function () {
-    sinon.stub(Model, 'create').resolves(newCarMongoResponse);
-
-    const service = new CarService();
-    const result = await service.insertOneCar(newCarPost);
-
-    expect(result).to.be.deep.equal(newCarPostResponse);
-  });
-
-  it('Buscando todos os carros', async function () {
-    sinon.stub(Model, 'find').resolves(carListMongoResponse);
-
-    const service = new CarService();
-    const result = await service.findAll();
-
-    expect(result).to.be.deep.equal(carListGetResponse);
-  });
-
-  it('Buscando um carro por id ', async function () {
-    sinon.stub(Model, 'findById').resolves(newCarMongoResponse);
-
-    const service = new CarService();
-    const result = await service.findById('6448542ef7475359162577f1');
-
-    expect(result).to.be.deep.equal(newCarPostResponse);
-  });
-
-  it('Atualizando um carro por id', async function () {
-    sinon.stub(Model, 'findByIdAndUpdate').resolves(newCarUpdatedMongoResponse);
-
-    const service = new CarService();
-    const result = await service.updateOne('6448542ef7475359162577f1', newCarUpdatedPost);
-
-    expect(result).to.be.deep.equal(newCarUpdatedPost);
-  });
-
-  it('Remover um carro por id', async function () {
-    sinon.stub(Model, 'findByIdAndDelete').resolves(newCarMongoResponse);
-
-    const service = new CarService();
-    const result = await service.removeOne('6448542ef7475359162577f1');
-
-    expect(result).to.be.deep.equal(newCarPostResponse);
-  });
+describe('Test CarService', function () {
   afterEach(function () {
     sinon.restore();
+  });
+
+  it('Registering new car', async function () {
+    sinon.stub(Model, 'create')
+      .resolves(newCarMongoResponse);
+    const service = new CarService();
+    const result = await service
+      .insertOneCar(newCarPost);
+    expect(result).to.be.deep
+      .equal(newCarPostResponse);
+  });
+
+  it('Searching all cars', async function () {
+    sinon.stub(Model, 'find')
+      .resolves(carListMongoResponse);
+    const service = new CarService();
+    const result = await service.findAll();
+    expect(result).to.be.deep
+      .equal(carListGetResponse);
+  });
+
+  it('Searching a car by id', async function () {
+    sinon.stub(Model, 'findById')
+      .resolves(newCarMongoResponse);
+    const service = new CarService();
+    const result = await service
+      .findById('6448542ef7475359162577f1');
+    expect(result).to.be.deep
+      .equal(newCarPostResponse);
+  });
+
+  it('Updating a car by id', async function () {
+    sinon.stub(Model, 'findByIdAndUpdate')
+      .resolves(newCarUpdatedMongoResponse);
+    const service = new CarService();
+    const result = await service
+      .updateOne(
+        '6448542ef7475359162577f1',
+        newCarUpdatedPost,
+      );
+    expect(result).to.be.deep
+      .equal(newCarUpdatedPost);
+  });
+
+  it('Remove a car by id', async function () {
+    sinon.stub(Model, 'findByIdAndDelete')
+      .resolves(newCarMongoResponse);
+    const service = new CarService();
+    const result = await service
+      .removeOne('6448542ef7475359162577f1');
+    expect(result).to.be.deep
+      .equal(newCarPostResponse);
   });
 });
--- tests/unit/Services/Motorcycles.test.ts
+++ tests/unit/Services/Motorcycles.test.ts
@@ -3,56 +3,69 @@ import sinon from 'sinon';
 import { Model } from 'mongoose';
 
 import MotorcycleService from '../../../src/Services/MotorcycleService';
-import { MotorCyclesListGetResponse, MotorCyclesListMongoResponse, newMotorCyclesMongoResponse, 
+import { 
+  MotorCyclesListGetResponse,
+  MotorCyclesListMongoResponse,
+  newMotorCyclesMongoResponse, 
   newMotorCyclesMongoUpdatedResponse, 
-  newMotorCyclesPost, newMotorCyclesPostResponse, 
-  newMotorCyclesUpdatedPost } from './mocks/MotorcyclesMock';
+  newMotorCyclesPost,
+  newMotorCyclesPostResponse, 
+  newMotorCyclesUpdatedPost,
+} from './mocks/MotorcyclesMock';
 
-describe('Testa a camada MotorcycleService', function () {
-  it('Cadastrando nova moto', async function () {
-    sinon.stub(Model, 'create').resolves(newMotorCyclesMongoResponse);
-
-    const service = new MotorcycleService();
-    const result = await service.insertOneMotorcycle(newMotorCyclesPost);
-
-    expect(result).to.be.deep.equal(newMotorCyclesPostResponse);
-  });
-
-  it('Buscando todos as motos', async function () {
-    sinon.stub(Model, 'find').resolves(MotorCyclesListMongoResponse);
-
-    const service = new MotorcycleService();
-    const result = await service.findAll();
-
-    expect(result).to.be.deep.equal(MotorCyclesListGetResponse);
-  });
-  it('Buscando um carro por id ', async function () {
-    sinon.stub(Model, 'findById').resolves(newMotorCyclesMongoResponse);
-
-    const service = new MotorcycleService();
-    const result = await service.findById('6348513f34c397abcad040b2');
-
-    expect(result).to.be.deep.equal(newMotorCyclesPostResponse);
-  });
-
-  it('Atualizando uma moto por id', async function () {
-    sinon.stub(Model, 'findByIdAndUpdate').resolves(newMotorCyclesMongoUpdatedResponse);
-
-    const service = new MotorcycleService();
-    const result = await service.updateOne('6348513f34c397abcad040b2', newMotorCyclesUpdatedPost);
-
-    expect(result).to.be.deep.equal(newMotorCyclesUpdatedPost);
-  });
-
-  it('Remover uma moto por id', async function () {
-    sinon.stub(Model, 'findByIdAndDelete').resolves(newMotorCyclesMongoResponse);
-
-    const service = new MotorcycleService();
-    const result = await service.removeOne('6348513f34c397abcad040b2');
-
-    expect(result).to.be.deep.equal(newMotorCyclesPostResponse);
-  });
+describe('Test MotorcycleService', function () {
   afterEach(function () {
     sinon.restore();
+  });
+
+  it('Registering new motorcycle', async function () {
+    sinon.stub(Model, 'create')
+      .resolves(newMotorCyclesMongoResponse);
+    const service = new MotorcycleService();
+    const result = await service
+      .insertOneMotorcycle(newMotorCyclesPost);
+    expect(result).to.be.deep
+      .equal(newMotorCyclesPostResponse);
+  });
+
+  it('Searching all motorcycle', async function () {
+    sinon.stub(Model, 'find')
+      .resolves(MotorCyclesListMongoResponse);
+    const service = new MotorcycleService();
+    const result = await service.findAll();
+    expect(result).to.be.deep
+      .equal(MotorCyclesListGetResponse);
+  });
+
+  it('Searching a motorcycle by id', async function () {
+    sinon.stub(Model, 'findById')
+      .resolves(newMotorCyclesMongoResponse);
+    const service = new MotorcycleService();
+    const result = await service
+      .findById('6348513f34c397abcad040b2');
+    expect(result).to.be.deep
+      .equal(newMotorCyclesPostResponse);
+  });
+
+  it('Updating a motorcycle by id', async function () {
+    sinon.stub(Model, 'findByIdAndUpdate')
+      .resolves(newMotorCyclesMongoUpdatedResponse);
+    const service = new MotorcycleService();
+    const result = await service.updateOne(
+      '6348513f34c397abcad040b2',
+      newMotorCyclesUpdatedPost,
+    );
+    expect(result).to.be.deep
+      .equal(newMotorCyclesUpdatedPost);
+  });
+
+  it('Remove a motorcycle by id', async function () {
+    sinon.stub(Model, 'findByIdAndDelete')
+      .resolves(newMotorCyclesMongoResponse);
+    const service = new MotorcycleService();
+    const result = await service
+      .removeOne('6348513f34c397abcad040b2');
+    expect(result).to.be.deep
+      .equal(newMotorCyclesPostResponse);
   });
 });
