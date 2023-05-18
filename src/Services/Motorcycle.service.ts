8bfc48b6b0bab443a0d26e9170eb0846f5e85bd9
Luana Portezan <luanaportezan@gmail.com>
2023-05-18 08:05:20 -0300

    feat: service motorcycles get all, get by Id, update and remove

--- src/Services/Motorcycle.service.ts
+++ src/Services/Motorcycle.service.ts
@@ -14,9 +14,32 @@ class MotorcycleService {
     }
   }
 
-  public async insertOneMotorcycle(motorcycle: IMotorcycle): | Promise<Motorcycle | undefined> {
+  public async insertOneNewMotorcycle(motorcycle: IMotorcycle): | Promise<Motorcycle | undefined> {
     const result = await this.motorcycleODM.create(motorcycle);
     return this.createMotorcycleDomain(result);
   }
+
+  public async findAllMotorcycle(): Promise<(Motorcycle | undefined)[] | undefined> {
+    const result = await this.motorcycleODM.findAll();
+    const motorcycleList = result?.map((motorcycle) => this.createMotorcycleDomain(motorcycle));
+    return motorcycleList;
+  }
+
+  public async findByIdMotorcycle(id: string):Promise <Motorcycle | undefined> {
+    const result = await this.motorcycleODM.findById(id);
+    return this.createMotorcycleDomain(result);
+  }
+
+  public async updateOneMotorcycle(id: string, motorcycle: IMotorcycle)
+    : Promise<Motorcycle | undefined> {
+    const result = await this.motorcycleODM.update(id, motorcycle);
+    return this.createMotorcycleDomain(result);
+  }
+  public async removeOneMotorcycle(id:string): Promise <Motorcycle | undefined> {
+    const result = await this.motorcycleODM.remove(id);
+
+    return this.createMotorcycleDomain(result);
+  }
 }
+
 export default MotorcycleService;
