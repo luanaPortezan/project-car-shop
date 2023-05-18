343ad66d9d2c3a07e4148386dbdc34fea7206829
Luana Portezan <luanaportezan@gmail.com>
2023-05-18 06:57:47 -0300

    test: service car - update and remove

--- tests/unit/Services/Mocks/car.mock.ts
+++ tests/unit/Services/Mocks/car.mock.ts
@@ -67,7 +67,6 @@ export const carListGetResponse = [
     seatsQty: 5,
     status: true,
   },
-
   {
     id: '6448578e4bd27d324f4f8d46',
     model: 'Gol',
@@ -79,3 +78,26 @@ export const carListGetResponse = [
     status: true,
   },
 ];
+
+export const newCarUpdatedPost = {
+  model: 'Marea',
+  year: 2003,
+  color: 'Black',
+  status: true,
+  buyValue: 12.990,
+  doorsQty: 4,
+  seatsQty: 5,
+  id: '6448542ef7475359162577f1',
+};
+
+export const newCarUpdatedMongoResponse = {
+  model: 'Marea',
+  year: 2003,
+  color: 'Black',
+  status: true,
+  buyValue: 12.990,
+  doorsQty: 4,
+  seatsQty: 5,
+  id: '6448542ef7475359162577f1',
+  __v: 0,
+};
--- tests/unit/Services/car.test.ts
+++ tests/unit/Services/car.test.ts
@@ -2,7 +2,8 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 import { Model } from 'mongoose';
 import { carListGetResponse, carListMongoResponse, 
-  newCarMongoResponse, newCarPost, newCarPostResponse } from './Mocks/car.mock';
+  newCarMongoResponse, newCarPost, newCarPostResponse, 
+  newCarUpdatedMongoResponse, newCarUpdatedPost } from './Mocks/car.mock';
 import CarService from '../../../src/Services/Car.service';
 
 describe('Car Service Test', function () {
@@ -23,10 +24,26 @@ describe('Car Service Test', function () {
     expect(result).to.be.deep.equal(carListGetResponse);
   });
 
-  it('Searching a car by id', async function () {
+  it('Searching a car by Id', async function () {
     sinon.stub(Model, 'findById').resolves(newCarMongoResponse);
     const service = new CarService();
     const result = await service.findByIdCar('6448542ef7475359162577f1');
+    expect(result).to.be.deep.equal(newCarPostResponse);
+  });
+
+  it('Updating a car by Id', async function () {
+    sinon.stub(Model, 'findByIdAndUpdate').resolves(newCarUpdatedMongoResponse);
+    const service = new CarService();
+    const result = await service.updateOneCar('6448542ef7475359162577f1', newCarUpdatedPost);
+    expect(result).to.be.deep.equal(newCarUpdatedPost);
+  });
+
+  it('Remove a car by Id', async function () {
+    sinon.stub(Model, 'findByIdAndDelete').resolves(newCarMongoResponse);
+
+    const service = new CarService();
+    const result = await service.removeOneCar('6448542ef7475359162577f1');
+
     expect(result).to.be.deep.equal(newCarPostResponse);
   });
 });
