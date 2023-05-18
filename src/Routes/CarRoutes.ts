df14409582b934abdcd37821dc53ffe6e2fdcb99
Luana Portezan <luanaportezan@gmail.com>
2023-05-18 12:12:30 -0300

    refactor: routes

--- src/Routes/CarRoutes.ts
+++ src/Routes/CarRoutes.ts
@@ -4,19 +4,34 @@ import CarController from '../Controllers/CarController';
 
 const routes = Router();
 
-routes.post('/', (req, res, next) => 
-  new CarController(req, res, next).insertOneCar());
-
-routes.get('/:id', (req, res, next) => 
-  new CarController(req, res, next).findById());
-
-routes.put('/:id', (req, res, next) => 
-  new CarController(req, res, next).updateById());
-
-routes.get('/', (req, res, next) => 
-  new CarController(req, res, next).findAll());
-
-routes.delete('/:id', (req, res, next) => 
-  new CarController(req, res, next).removeById());
-
+routes.post(
+  '/',
+  (req, res, next) => new 
+  CarController(req, res, next)
+    .insertOneCar(),
+);
+routes.get(
+  '/:id',
+  (req, res, next) => new
+  CarController(req, res, next)
+    .findById(),
+);
+routes.put(
+  '/:id',
+  (req, res, next) => new
+  CarController(req, res, next)
+    .updateById(),
+);
+routes.get(
+  '/',
+  (req, res, next) => new
+  CarController(req, res, next)
+    .findAll(),
+);
+routes.delete(
+  '/:id',
+  (req, res, next) => new 
+  CarController(req, res, next)
+    .removeById(),
+);
 export default routes;
--- src/Routes/MotorcyclesRoutes.ts
+++ src/Routes/MotorcyclesRoutes.ts
@@ -4,19 +4,35 @@ import MotorcycleController from '../Controllers/MotorcycleController';
 
 const routes = Router();
 
-routes.post('/', (req, res, next) => 
-  new MotorcycleController(req, res, next).insertOneMotorcycle());
-
-routes.get('/:id', (req, res, next) => 
-  new MotorcycleController(req, res, next).findById());
-
-routes.put('/:id', (req, res, next) => 
-  new MotorcycleController(req, res, next).updateById());
-
-routes.get('/', (req, res, next) => 
-  new MotorcycleController(req, res, next).findAll());
-
-routes.delete('/:id', (req, res, next) => 
-  new MotorcycleController(req, res, next).removeById());
+routes.post(
+  '/',
+  (req, res, next) => new
+  MotorcycleController(req, res, next)
+    .insertOneMotorcycle(),
+);
+routes.get(
+  '/:id',
+  (req, res, next) => new
+  MotorcycleController(req, res, next)
+    .findById(),
+);
+routes.put(
+  '/:id',
+  (req, res, next) => new
+  MotorcycleController(req, res, next)
+    .updateById(),
+);
+routes.get(
+  '/',
+  (req, res, next) => new
+  MotorcycleController(req, res, next)
+    .findAll(),
+);
+routes.delete(
+  '/:id',
+  (req, res, next) => new
+  MotorcycleController(req, res, next)
+    .removeById(),
+);
 
 export default routes;
